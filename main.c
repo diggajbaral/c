eac3f66f94a7a5443b5d4223c544dbcf0a0a6006
diggajbaral <volaribrl@gmail.com>
2025-09-11 01:22:21 -0700

    guess the code game

--- main.c
+++ main.c
@@ -2,7 +2,24 @@
 
 int main() {
 
-      
+    int guess, code = 7;
+
+    while (1) {
+
+        printf("enter the code : ");
+        scanf("%d", &guess);
+
+        if (guess == code) {
+
+            while (1) {
+                printf("*");
+            }
+            
+        } else {
+            printf("try again\n");
+        }
+
+    }
 
     return 0;
 }
