4bfaa0a1b5d8b064de26b24c6778e947b5401801
diggajbaral <volaribrl@gmail.com>
2025-09-14 04:53:46 -0700

    array in string

--- main.c
+++ main.c
@@ -2,20 +2,8 @@
 
 int main() {
 
-    int number[5];
-    int sum = 0;
-
-    printf("Enter 5 integer : \n");
-    for (int i = 0; i < 5; i++) {
-        printf("enter number %d: ", i + 1);
-        scanf("%d", &number[i]);
-    }
-
-    for (int i = 0; i < 5; i++) {
-        sum += number[i];
-    }
-
-    printf("sum of array element is : %d\n", sum);
+    char name[] = "Alice";
+    printf("Name %s", name);
 
     return 0;
 }
