a7cddb31732da3edfe3e74b230ed7c0410c99b3d
diggajbaral <volaribrl@gmail.com>
2025-12-31 14:31:03 +0530

    Can vote or not

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
-
-int add(int a, int b) {
-    return a + b;
-}
+#include <math.h>
 
 int main() {
-    int result = add(5, 3);
-    printf("Sum = %d\n", result);
+
+    int age;
+    printf("Enter age : ");
+    scanf("%d", &age);
+
+    if (age > 18){
+        printf("Can Vote\n");
+    } else {
+        printf("Can't Vote");
+    }
+
     return 0;
+
 }
